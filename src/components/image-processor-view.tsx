cc0107d130eb2ef634fe51aa07684acb45f48ded
Mayur Hande <mayurhande90@gmail.com>
2025-10-07 05:03:25 +0000

    Try fixing this error: `Runtime Error: Error: Link is not defined. Error

--- src/components/image-processor-view.tsx
+++ src/components/image-processor-view.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useState } from 'react';
+import Link from 'next/link';
 import type { Feature } from '@/lib/types';
 import { features } from '@/lib/features';
 import { FileUploader } from '@/components/file-uploader';
