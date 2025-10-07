93f9fe9ccf994a696416b7902de896ed289e27fb
Mayur Hande <mayurhande90@gmail.com>
2025-10-07 09:48:16 +0000

    still not able to see the images.

--- next.config.ts
+++ next.config.ts
@@ -18,26 +18,18 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'storage.googleapis.com',
-        port: '',
-        pathname: '/**',
       },
     ],
   },
